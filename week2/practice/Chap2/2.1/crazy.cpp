b889a83635c218162d56b8a69636088ff96adf7a
Alexander Miranda <alexandermichaelmiranda@gmail.com>
2015-04-04 12:10:54 -0700

    Week 2 exercises

---
+++ week2/practice/Chap2/2.1/crazy.cpp
@@ -0,0 +1,16 @@
+/**************************************
+  Name: Alexander Miranda
+  Date: April 4, 2015
+  Description: A crazy mixed up program
+***************************************/
+
+// A crazy mixed up program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "In 1492 Columbus sailed the ocean blue." << endl;
+  return 0;
+}
---
+++ week2/practice/Chap2/2.2/myNameIs.cpp
@@ -0,0 +1,16 @@
+/*****************************************
+  Name: Alexander Miranda
+  Date: April 4, 2015
+  Description: Program that prints my name
+*****************************************/
+
+// A program that outputs my name
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Alexander Miranda" << endl;
+  return 0;
+}
